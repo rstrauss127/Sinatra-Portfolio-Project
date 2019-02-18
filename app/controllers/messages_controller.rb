ab90b4741558e3cc8e83a330007182b95a722b1d
Rebecca Strauss <rstrauss127@gmail.com>
2019-02-18 18:02:36 +0000

    'post/:id' correctly displaying author of post, added new message form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,11 +4,18 @@ class MessagesController < ApplicationController
     #show messages
   end
 
+  post '/messages' do
+    @messages = Messages.create(params)
+    @messages.sender = current_user.id
+    @messages.recipent
+  end
+
   get '/messages/new' do
     if !logged_in?#checking if logged in
       redirect "/login" #redirect if not
     else
       erb :"/messages/new" #render new message form if they are logged in
+      puts params
     end
   end
 
