67efdf9ce33c14977511cde6cbc85ecb5c09afca
Rebecca Strauss <rstrauss127@gmail.com>
2019-02-25 18:27:00 +0000

    studysessions/index only shows users sztudysessions

--- app/controllers/study_sessions_controller.rb
+++ app/controllers/study_sessions_controller.rb
@@ -1,13 +1,12 @@
 class StudySessionsController < ApplicationController
   get '/study_sessions' do
+    @current_user = current_user
     @study_sessions = StudySession.all
     erb :"study_sessions/index"
   end
 
   post '/study_sessions' do
     @study_session = StudySession.create(params)
-    @study_session.user_id = current_user.id
-    @study_session.save
     redirect "/posts"
   end
 
@@ -15,6 +14,7 @@ class StudySessionsController < ApplicationController
     if !logged_in?#checking if logged in
       redirect "/login" #redirect if not
     else
+      @current_user = current_user
       erb :"/study_sessions/new" #render new post form if they are logged in
     end
   end
